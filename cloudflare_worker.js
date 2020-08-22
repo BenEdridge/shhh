571d78ec224ed503eba8896799c208fa719c047b
Ben Edridge <BenEdridge@users.noreply.github.com>
2020-08-22 19:31:02 +1000

    Basic POC

---
+++ cloudflare_worker.js
@@ -0,0 +1,193 @@
+// https://developers.cloudflare.com/workers/examples/cache-api
+// https://blog.cloudflare.com/introducing-the-workers-cache-api-giving-you-control-over-how-your-content-is-cached/
+
+let cache = caches.default;
+
+function rawHtmlResponse(html) {
+  const init = {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  }
+  return new Response(html, init)
+}
+
+async function readRequestBody(request) {
+  const { headers } = request
+  const contentType = headers.get("content-type") || ""
+
+  if (contentType.includes("application/json")) {
+    return JSON.stringify(await request.json())
+  }
+  else if (contentType.includes("application/text")) {
+    return await request.text()
+  }
+  else if (contentType.includes("text/html")) {
+    return await request.text()
+  }
+  else if (contentType.includes("form")) {
+    const formData = await request.formData()
+    const body = {}
+    for (const entry of formData.entries()) {
+      body[entry[0]] = entry[1]
+    }
+    return JSON.stringify(body)
+  }
+  else {
+    const myBlob = await request.blob()
+    const objectURL = URL.createObjectURL(myBlob)
+    return objectURL
+  }
+}
+
+const someForm = `
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+textarea {
+  width: 100%;
+  resize: vertical;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+}
+
+.radios {
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  width: fit-content;
+}
+
+input[type="radio"] {
+  width: 10px;
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  box-sizing: border-box;
+  margin-top: 10px;
+}
+
+input {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+}
+.container {
+  background-color: #f1f1f1;
+  padding: 20px;
+}
+
+</style>
+</head>
+<body>
+
+<div class="container">
+
+<h3>Secret Sharing</h3>
+
+  <form action="share" method="POST">
+    <label for="secret">Secret</label>
+    
+    <textarea id="secret" name="secret" rows="5" required></textarea>
+    
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+    
+    <label for="radios">Expiry</label>
+	  <div class="radios">
+
+      <input id=120 type="radio" name="expiry" value="120">
+      <label for="120">2 Minutes</label><br>
+
+      <input id=900 type="radio" name="expiry" value="900">
+      <label for="120">15 Minutes</label><br>
+
+      <input id=86400 type="radio" name="expiry" value="86400">
+      <label for="120">24 Hours</label><br>
+    </div>
+    <input type="submit" value="Share">
+  </form>
+</div>
+</body>
+</html>
+`;
+
+function generateRandom(){
+  return '1234567';
+}
+
+async function handleShareRequest(event) {
+
+  const { request } = event;
+
+  const cacheUrl = new URL(request.url)
+  const randomisedCacheUrl = `https://polished-term-6b3e.boost.workers.dev/reveal/${generateRandom()}`;
+
+  const reqBody = await readRequestBody(request);
+  let response = await cache.match(randomisedCacheUrl)
+
+  if (!response) {
+
+    response = new Response(reqBody);
+
+    // Cache API respects Cache-Control headers. Setting max-age to 10
+    // will limit the response to be in cache for 10 seconds max
+    response.headers.append("Cache-Control", "max-age=30")
+
+    // Store the fetched response as cacheKey
+    // Use waitUntil so computational expensive tasks don"t delay the response
+    event.waitUntil(cache.put(randomisedCacheUrl, response.clone()))
+    return new Response(randomisedCacheUrl);
+
+  } else {
+    return new Response('This secret is stored please use the /reveal endpoint to consume it');
+  }
+}
+
+async function handleRevealRequest(request) {
+
+  const cacheUrl = new URL(request.url);
+  const randomisedCacheUrl = `https://polished-term-6b3e.boost.workers.dev/reveal/${generateRandom()}`;
+
+  let response = await cache.match(randomisedCacheUrl);
+
+  if (!response) {
+    return new Response('URL is invalid or secret has expired from cache');
+  } else {
+    return response;
+  }
+};
+
+addEventListener("fetch", event => {
+  const { request } = event
+  const { url } = request
+
+  if (request.method === "GET") {
+    if(url.includes("/reveal")){
+      return event.respondWith(handleRevealRequest(request))
+    } else {
+      return event.respondWith(rawHtmlResponse(someForm))
+    }
+
+  } else if (request.method === "POST" && url.includes("/share")) {
+    return event.respondWith(handleShareRequest(event));
+  }
+});
